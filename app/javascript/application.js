e1dc00b99ca3a2dc502cc564eabb2b2fa63b1d37
Mathieu VALLEAU <mathieuvalleau@hotmail.fr>
2024-02-05 09:08:09 +0100

    Pas de filtre mais rajout geocoder

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,64 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+const TOKEN = "pk.eyJ1IjoibWF0dmE4NCIsImEiOiJjbHIyYnFpMW8wejN1MnJvMXUxcHdqaDM2In0.-PHRqAW9yKRl1beEI4AOvg";
+
+//const showMap = (userInput) => {
+//  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${userInput}.json?access_token=${token}`;
+//  fetch(url)
+//    .then((response) => response.json())
+//    .then((data) => {
+//      alert(userInput);
+//      const disp = document.querySelector(".font-monospace");
+//      disp.innerText = data.features[0].geometry.coordinates;
+      // - Create a map using the Mapbox API and the coordinates
+      // - Add a marker to the map at the coordinates
+//    });
+//};
+
+
+//showMap(address.innerText)
+
+
+
+//const TOKEN = "your-token-here";
+
+const displayCoordinates = (longitude, latitude) => {
+  const paragraphElement = document.getElementById("coordinates");
+  paragraphElement.innerText = `${latitude}, ${longitude}`;
+};
+
+const injectMap = (lng, lat) => {
+  const mapDiv = document.getElementById("map");
+  mapDiv.innerHTML = "";
+
+  mapboxgl.accessToken = TOKEN;
+  const map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v9",
+    center: [lng, lat],
+    zoom: 12,
+  });
+  new mapboxgl.Marker().setLngLat([lng, lat]).addTo(map);
+};
+
+//const showMapAndCoordinates = (event) => {
+//  event.preventDefault();
+  const address = document.getElementById("address").innerText;
+  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=${TOKEN}`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then((data) => {
+      const longitude = data.features[0].center[0];
+      const latitude = data.features[0].center[1];
+      //displayCoordinates(longitude, latitude);
+      console.log(address);
+      console.log(longitude);
+      console.log(latitude);
+      injectMap(longitude, latitude);
+    });
+//};
+
+//showMapAndCoordinates;
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,48 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require "faker"
+
+urls = [
+  "https://images.unsplash.com/photo-1564013799919-ab600027ffc6?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1518780664697-55e3ad937233?q=80&w=2865&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1570129477492-45c003edd2be?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1568605114967-8130f3a36994?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1600585154340-be6161a56a0c?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?q=80&w=2950&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1523217582562-09d0def993a6?q=80&w=2960&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1512917774080-9991f1c4c750?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1576941089067-2de3c901e126?q=80&w=2778&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1480074568708-e7b720bb3f09?q=80&w=2948&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1564013799919-ab600027ffc6?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1575517111478-7f6afd0973db?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1572120360610-d971b9d7767c?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1598228723793-52759bba239c?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1448630360428-65456885c650?q=80&w=2934&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1513584684374-8bab748fbf90?q=80&w=2930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1605276374104-dee2a0ed3cd6?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1574620845949-61a08a005988?q=80&w=2825&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1582268611958-ebfd161ef9cf?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1571939228382-b2f2b585ce15?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1560026301-88340cf16be7?q=80&w=2788&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1625602812206-5ec545ca1231?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+]
+
+puts "Cleaning database..."
+Flat.destroy_all
+
+puts "Creating flats..."
+
+15.times do
+  flat = Flat.new(
+    name: Faker::Artist.name,
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.sentences,
+    price_per_night: rand(20..100),
+    number_of_guests: rand(2..8),
+    picture_url: urls[rand(0..20)]
+  )
+  flat.save
+end
+
+puts "Finished"
